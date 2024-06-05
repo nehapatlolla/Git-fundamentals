53cca6b8b8d51b8865626333b767787b24745ec4
nehapatlolla <neha.patlolla@proclink.com>
2024-06-05 15:09:46 +0530

    change the function

--- function1.js
+++ function1.js
@@ -1,10 +1,10 @@
 // Define a function to calculate the sum of two numbers
-function sum(a, b) {
-    return a + b;
+function summy(hi, hell) {
+    return hi + hell;
 }
 
 // Call the sum function with two numbers and store the result
-let result = sum(5, 3);
+let res= sum(6, 7);
 
 // Print the result
 console.log("The sum is:", result);
